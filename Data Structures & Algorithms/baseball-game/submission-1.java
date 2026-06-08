4d052a0c12e0117cf8d2cc0ac630978bdbb527cd
Alibek Khudoykulov <66941344+AlibekKhudoykulov@users.noreply.github.com>
2026-06-08 09:59:28 +0500

    Add: baseball-game - submission-1

---
+++ Data Structures & Algorithms/baseball-game/submission-1.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int calPoints(String[] operations) {
+        int res = 0;
+        Deque<Integer> stack = new ArrayDeque<>();
+        for (String operation : operations) {
+            switch (operation) {
+                case "C":
+                    stack.pop();
+                    break;
+                case "D":
+                    stack.push(stack.peek() * 2);
+                    break;
+                case "+":
+                    int top = stack.pop();
+                    int newTop = stack.peek();
+                    stack.push(top);
+                    stack.push(newTop);
+                    break;
+                default:
+                    stack.push(Integer.parseInt(operation));
+                    break;
+            }
+        }
+        for (Integer score : stack) {
+            res += score;
+        }
+        return res;
+    }
+}
